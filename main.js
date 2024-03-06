477dc41b06274618417b92a21e55ffb6d1c42cad
nutzu707 <59876715+nutzu707@users.noreply.github.com>
2024-03-06 09:42:50 -0500

    aoskndasokndasokndsaodnasosadnok

--- main.js
+++ main.js
@@ -21,3 +21,62 @@ function ChangeBtnColor(btnId){
     PrevBtn=btnId;
 }
 
+PrevBtn2='firstDate';
+function ChangeResBtn(btnId){
+    document.getElementById(PrevBtn2).style.backgroundColor="#FCF6F6";
+    document.getElementById(PrevBtn2).style.color="#1F1E20";
+
+    document.getElementById(btnId).style.backgroundColor="#1F1E20";
+    document.getElementById(btnId).style.color="#FCF6F6";
+
+    PrevBtn2=btnId;
+
+
+}
+
+
+PrevBtn3='firstHour';
+function ChangeHourBtn(btnId){
+    document.getElementById(PrevBtn3).style.backgroundColor="#FCF6F6";
+    document.getElementById(PrevBtn3).style.color="#1F1E20";
+
+    document.getElementById(btnId).style.backgroundColor="#1F1E20";
+    document.getElementById(btnId).style.color="#FCF6F6";
+
+    PrevBtn3=btnId;
+
+
+    document.getElementById('time').style.innerHTML='04 - 07 PM';
+
+}
+
+
+
+
+Adults=1;
+function addOneAdult(){
+    if(Adults<30)
+        Adults++;
+    document.getElementById('adultCounter').innerText=Adults;
+}
+
+function removeOneAdult(){
+    if(Adults>1)
+        Adults--;
+    document.getElementById('adultCounter').innerText=Adults;
+}
+
+
+
+kids=1;
+function addOneKid(){
+    if(kids<30)
+        kids++;
+    document.getElementById('kidsCounter').innerText=kids;
+}
+
+function removeOneKid(){
+    if(kids>1)
+        kids--;
+    document.getElementById('kidsCounter').innerText=kids;
+}
